4229763ffe469b3be57f1799226f4f7a8ec8c802
shuheizzzz <51748038+shuheizzzz@users.noreply.github.com>
2019-09-07 09:15:34 +0900

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-  <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes"><!-- for smartphone. ここは一旦、いじらなくてOKです。 -->
+  <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
 	<meta charset="utf-8">
 	<title>Test</title>
 </head>
 <center>
     <body>
-        <form action="mission_2-3.php" method="POST" enctype="multipart/form-data">
+        <form action="test3.php" method="POST" enctype="multipart/form-data">
             NAME：<input type="text" name="name"><br>
             <input type="submit" value="SEND"><br>
         </form>
@@ -14,14 +14,14 @@
     <?php
         $comment = @$_POST["name"];
         if(!empty($comment)){ 
-            $filename = "mission_2-3.txt";
+            $filename = "test3.txt";
             $fp = fopen($filename, "a");
             fwrite($fp, $comment."<br>");
             fclose($fp);
         }
     ?>
     <?php
-        $fp = fopen("mission_2-3.txt", "r");
+        $fp = fopen("test3.txt", "r");
         while ($temp = fgets($fp)) {
             if(!empty($temp)){
                 echo "$temp<br>";

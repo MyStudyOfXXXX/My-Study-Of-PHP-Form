910de3ca55870f9a9a881bc63dca7a20bf265e83
shuheizzzz <51748038+shuheizzzz@users.noreply.github.com>
2019-09-07 09:14:02 +0900

    Update and rename mission_2-4.php to test4.php

--- test4.php
+++ test4.php
@@ -1,22 +1,19 @@
 <html>
 <head>
-  <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes"><!-- for smartphone. ここは一旦、いじらなくてOKです。 -->
-	<meta charset="utf-8"><!-- 文字コード指定。ここはこのままで。 -->
+  <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
+	<meta charset="utf-8">
 	<title>Test</title>
 </head>
 <center>
     <body>
         <form action="mission_2-4.php" method="POST" enctype="multipart/form-data">
-            名前：<input type="text" name="name"><br>
+            NAME：<input type="text" name="name"><br>
             <input type="submit" value="SEND"><br>
         </form>
     </body>
     <?php
         $comment = @$_POST["name"];
-        if(!empty($comment)){  
-            if($comment == "完成！"){
-                $comment = "おめでとう！";
-            }
+        if(!empty($comment)){ 
             $filename = "mission_2-4.txt";
             $fp = fopen($filename, "a");
             fwrite($fp, $comment."<br>");

b339c285eb930e4a2092709201eacd5795ed3c10
shuheizzzz <51748038+shuheizzzz@users.noreply.github.com>
2019-09-17 16:59:35 +0900

    Update and rename mission_3-4.php to test8.php

--- test8.php
+++ test8.php
@@ -2,21 +2,21 @@
 <head>
   <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
 	<meta charset="utf-8">
-	<title>掲示板</title>
+	<title>test</title>
 </head>
 <center>
     <body>
-        <form action="mission_3-4.php" method="POST" enctype="multipart/form-data">
-            名前：<input type="text" name="name"><br>
-            コメント：<textarea name="comment"></textarea><br>
-            <input type="submit" value="送信"><br>
+        <form action="test8.php" method="POST" enctype="multipart/form-data">
+            NAME：<input type="text" name="name"><br>
+            COMMENT：<textarea name="comment"></textarea><br>
+            <input type="submit" value="SEND"><br>
         </form>
         <?php
 
         $name = @$_POST["name"];
         $comment = @$_POST["comment"];
         
-        $filename = "mission_3-4.txt";
+        $filename = "test8.txt";
 
         $y = date("Y");
         $m = date("m");
@@ -35,14 +35,14 @@
             fclose($fp);
         }
         
-        $file_array = file('mission_3-4.txt');
+        $file_array = file('test8.txt');
 
         foreach($file_array as $value){  
             $file_array_ex = explode( '<>', $value );
             $count = $file_array_ex[0];
         }
         ?>
-        <form action="mission_3-4.php" method="POST" enctype="multipart/form-data">
+        <form action="test8.php" method="POST" enctype="multipart/form-data">
             <select name="delete" id="delete">
             <?php
             for ($j = 1; $j <= $count; $j++) {
@@ -50,7 +50,7 @@
             }
             ?>
             </select>
-            <input type="submit" value="削除"><br>
+            <input type="submit" value="DELETE"><br>
             <select name="edit" id="edit">
             <?php
             for ($j = 1; $j <= $count; $j++) {
@@ -58,7 +58,7 @@
             }
             ?>
             </select>
-            <input type="submit" value="編集"><br>
+            <input type="submit" value="EDIT"><br>
         </form>
         <?php
         
@@ -108,10 +108,10 @@
             }
             if( $edit_name == $file_array_ex[0] ){
                 echo ("<br>");
-                echo ("<form action='mission_3-4.php' method='POST' enctype='multipart/form-data'>
-                名前：<input type='text' name='name_edit'><br>
-                コメント：<textarea name='comment_edit'></textarea><br>
-                <input type='submit' value='送信'><br>
+                echo ("<form action='test8.php' method='POST' enctype='multipart/form-data'>
+                NAME：<input type='text' name='name_edit'><br>
+                COMMENT：<textarea name='comment_edit'></textarea><br>
+                <input type='submit' value='SEND'><br>
                 </form>");
                 echo ("<br>");
 
